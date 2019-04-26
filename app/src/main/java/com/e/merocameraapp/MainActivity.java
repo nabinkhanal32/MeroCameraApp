932d7833d73c0f41f0958b3b5b230ac3fd643d85
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:13:00 +0545

    Initial commit

---
+++ app/src/main/java/com/e/merocameraapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.e.merocameraapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView imgView;
+    private Button btnImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imgView);
+        btnImage = findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(this);
+        checkPermission();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        loadCamera();
+    }
+
+    private void loadCamera() {
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    private void checkPermission()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK)
+        {
+            Bundle extra = data.getExtras();
+            Bitmap bitmap =(Bitmap) extra.get("data");
+            imgView.setImageBitmap(bitmap);
+        }
+    }
+}
